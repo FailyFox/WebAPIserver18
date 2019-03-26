24274af0b94a2e3eb30e634cce6a8e6649698c0e
ut_mat <ut_mat@A33COMP72>
2019-03-26 19:17:09 +0200

    changes added

---
+++ WebAPIworkers/Factory.cs
@@ -0,0 +1,54 @@
+namespace WebAPIworkers
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    /// <summary>
+    /// Class-constext for DataBase
+    /// </summary>
+
+    public class Factory : DbContext
+    {
+        /// <summary>
+        /// Constructor method for DB context class
+        /// </summary>
+        public Factory()
+            : base("name=Factory")
+        {
+            Database.SetInitializer<Factory>(new CustomInit<Factory>());
+        }
+
+        /// <summary>
+        /// Table for Workers
+        /// </summary>
+        public virtual DbSet<Worker> Workers { get; set; }
+    }
+
+    /// <summary>
+    /// Model class for "Worker" entity
+    /// </summary>
+    public class Worker
+    {
+        /// <summary>
+        /// Primary key
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Worker's first name
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Worker's last name
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Worker's age
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// Worker's salary, decimal type
+        /// </summary>
+        public decimal Salary { get; set; }
+    }
+}
